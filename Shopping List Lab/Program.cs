4f02f07c81cea851c61c3da5fc8c9c3147413c4e
alexis <alexisd.horton@gmail.com>
2021-11-23 17:14:15 -0500

    New

---
+++ Shopping List Lab/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shopping_List_Lab
+{
+    class Program
+    {
+        static bool GoAgain(string message)
+        {
+            while (true)
+            {
+                Console.WriteLine(message);
+                string entry = Console.ReadLine();
+                if (entry.ToLower() == "n")
+                {
+                    return false;
+                }
+                if (entry.ToLower() == "y")
+                {
+                    return true;
+                }
+                Console.WriteLine("I'm sorry, I don't know what that means.");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<string> shoppingList = new List<string>();
+
+            Dictionary<string, decimal> cost = new Dictionary<string, decimal>();
+            cost["mango"] = 0.79m;
+            cost["carrot"] = 1.59m;
+            cost["apple"] = 0.89m;
+            cost["banana"] = 0.59m;
+            cost["bread"] = 1.29m;
+            cost["eggs"] = 2.29m;
+            cost["water"] = 3.29m;
+            cost["coffee"] = 5.69m;
+
+            do
+            {
+
+                Console.WriteLine("Hello! Welcome to the Market!");
+
+                foreach (var pair in cost)
+                {
+                    Console.WriteLine($" Item \n {pair.Key}      Cost {pair.Value}");
+                }
+
+                bool validEntry = false;
+                do
+                {
+                    Console.Write("\nEnter your grocery item: ");
+                    string entry = Console.ReadLine().ToLower();
+                    if (cost.ContainsKey(entry))
+                    {
+                        Console.WriteLine($"\nYou selected {entry} which costs ${cost[entry]}.");
+                        Console.WriteLine($"Adding {entry} to cart at ${cost[entry]}");
+                        shoppingList.Add(entry);
+                        validEntry = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Whoops, that item is MIA! Please try again.");
+                    }
+                } while (validEntry == false);
+
+            } while (GoAgain("Would you like to add an item to your order? (y/n)"));
+
+            Console.WriteLine("Thanks for your order!");
+            Console.WriteLine("Here's what you got: ");
+
+            foreach (string item in shoppingList)
+            {
+                Console.WriteLine(item);
+                for (int i=0; i<shoppingList.Count; i++)
+                {
+                    Console.WriteLine("The total cost of your items is ");
+                }
+            }
+        }
+    }
+}
