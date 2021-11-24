b8943232af03649568535093de36ac74084e1eab
alexis <alexisd.horton@gmail.com>
2021-11-24 14:37:50 -0500

    Wed

---
+++ ListMemoryDem2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListMemoryDem2
+{
+    class Program
+    {
+        static void Modify(List<string> mylist)
+        {
+            mylist.Add("Frank");
+        }
+
+        static void TestInt(int n)
+        {
+            n++;
+            Console.WriteLine(n);
+        }
+
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+            names.Add("Fred");
+            names.Add("Sally");
+
+            Modify(names);
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            int x = 10;
+            TestInt(x);
+            Console.WriteLine(x);
+        }
+
+        
+    }
+}
